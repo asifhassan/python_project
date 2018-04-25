f1252daceea295ce0c8f1d259208f9de07621e29
asifhassan <asif08ece@gmail.com>
2018-04-25 02:17:40 -0600

    the editing of gaussian noising

--- gauss_noising.py
+++ gauss_noising.py
@@ -14,17 +14,11 @@ import matplotlib.pyplot as plt
 ##      noisy = image + gauss
 ##      return noisy
 image=cv2.imread('pic_10.jpg',0)
-row=len(image)
-col=len(image[0])
-mean=0
-var=0.1
-sigma=var**0.5
-gauss = np.random.normal(mean,sigma,(row,col))
-gauss=gauss.reshape(row,col)
-noisy=image+gauss
+
+noisy=image+ 3 * image.std() * np.random.random(image.shape)
 cv2.imwrite('gaussian_pic10.png',noisy)
 cmap=plt.get_cmap('gray')
 plt.imshow(noisy,cmap)
-##img1=cv2.imread('gaussian_pic10.png',0)
+#cv2.imwrite('gaussian_pic10.png',noisy)
 ##plt.hist(img1.ravel(),256,[0,256])
 plt.show()
